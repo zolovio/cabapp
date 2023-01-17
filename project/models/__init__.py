cc1f164e9ea877590dc3fc2b0515048fd09bbe09
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2023-01-17 19:12:04 +0500

    update __init__.py

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -1,1 +1,4 @@
 from .user_model import Role, Gender, User, BlacklistToken, Licence, Vehicle, Location
+from .trip_model import TripStatus, RequestStatus, Trip, TripPassenger
+from .church_model import Church
+from .rating_model import Rating
